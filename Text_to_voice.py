7c67def07dc88dec32f28fb02dd1250119b687c5
ARUNAGIRINATHAN_K <167340745+ARUNAGIRINATHAN-K@users.noreply.github.com>
2025-06-24 15:42:21 +0000

    Class TextTOspeech

--- Text_to_voice.py
+++ Text_to_voice.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
 import pyttsx3
+
+
+class TextToSpeechApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Text to Speech Generator")
+        self.root.geometry("400x300")
+        
+        
